f3b165c9d7c50f1d2edb4bf89bf07d1f7677a193
kotjy <min.kotjy@gmail.com>
2023-08-22 15:37:25 +0800

    誤刪檔案 再次新增

---
+++ src/contexts/MainContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react";
+
+const MainContext = createContext();
+
+export const MainProvider = ({children}) => {
+      const [activeSection, setActiveSection] = useState('main')
+
+
+
+   const value ={
+    activeSection,
+    setActiveSection
+
+   }   
+ return <MainContext.Provider value ={value}>{children}</MainContext.Provider>
+}
+
+export const useMainFunction = () => {
+  const MainData = useContext(MainContext);
+  return MainData;
+};
