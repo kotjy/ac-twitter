04cb61bb60feded86f9dcf655ce6bcdf0080cb2b
kotjy <min.kotjy@gmail.com>
2023-08-23 16:21:19 +0800

    tweetInput

--- src/contexts/MainContext.jsx
+++ src/contexts/MainContext.jsx
@@ -3,8 +3,9 @@ import { createContext, useContext, useState } from "react";
 const MainContext = createContext();
 
 export const MainProvider = ({children}) => {
-    const [activeSection, setActiveSection] = useState('main')
-      
+    const [activeSection, setActiveSection] = useState('main');//顯示頁面
+    const [userData, setUserData] = useState();//使用者資料
+
     //以下為處理otherProfile的state 
     const [ otherUserData, setOtherUserData ] = useState([]);
 
@@ -12,8 +13,10 @@ export const MainProvider = ({children}) => {
    const value ={
     activeSection,
     setActiveSection,
+    userData,
+    setUserData,
     otherUserData,
-    setOtherUserData
+    setOtherUserData,
 
    }   
  return <MainContext.Provider value ={value}>{children}</MainContext.Provider>
